c59533e4619dd53f95d29ee5591fb44e905918af
maxpaperno <max@wdg.us>
2015-02-16 00:34:47 -0500

    Update common and custom generated headers for AutoQuad.

--- include/autoquad/version.h
+++ include/autoquad/version.h
@@ -1,12 +1,12 @@
 /** @file
  *	@brief MAVLink comm protocol built from autoquad.xml
- *	@see http://pixhawk.ethz.ch/software/mavlink
+ *	@see http://mavlink.org
  */
 #ifndef MAVLINK_VERSION_H
 #define MAVLINK_VERSION_H
 
-#define MAVLINK_BUILD_DATE "Thu Dec 26 12:53:28 2013"
+#define MAVLINK_BUILD_DATE "Mon Feb 16 00:18:34 2015"
 #define MAVLINK_WIRE_PROTOCOL_VERSION "1.0"
-#define MAVLINK_MAX_DIALECT_PAYLOAD_SIZE 254
+#define MAVLINK_MAX_DIALECT_PAYLOAD_SIZE 255
  
 #endif // MAVLINK_VERSION_H
